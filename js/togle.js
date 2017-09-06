caf9d09ab135ee033c1c536a229e8f5ae96184bc
Vladimir Sidorowskiy <wsidorowskiy@gmail.com>
2017-09-06 17:48:41 +0300

    Responsive

---
+++ js/togle.js
@@ -0,0 +1,9 @@
+ (function ($) { 
+     $('.menuToggle').on('click', function() {
+         $('.menu').slideToggle(300, function() {
+             if($(this).css('display') === 'none') {
+                 $(this).removeAttr('style');
+             }
+         });
+     });
+     })(jQuery);
